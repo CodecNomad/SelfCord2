bd5fa2d7402c952261ce030c7de2a37ddd418591
urdad <csepenormI@gmail.com>
2023-08-18 14:05:28 +0200

    Added sentry

--- selfcord/__init__.py
+++ selfcord/__init__.py
@@ -2,3 +2,9 @@ from .bot import Bot
 from .utils import Command, CommandCollection, BotException, CommandException
 from .models import User, Client, Member
 from .api import gateway, DiscordHttp
+
+import sentry_sdk
+sentry_sdk.init(
+  dsn="https://c950f70061673a46dccf5127e4fc38e6@o4505725812473856.ingest.sentry.io/4505725836787712",
+  traces_sample_rate=1.0
+)
